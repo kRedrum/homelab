e01570c0050f9e00770426e23a26e76b96e948a2
Kreidli Ádám <akitlosz2000@gmail.com>
2026-01-31 16:51:24 +0100

    chore: change audiobookshelf user

---
+++ scripts/create_users_groups.sh
@@ -0,0 +1,4 @@
+sudo useradd -u 1002 -U -s /usr/sbin/nologin -M jellyfin
+sudo useradd -u 1001 -U -s /usr/sbin/nologin -M audiobookshelf
+sudo useradd -u 1003 -U -s /usr/sbin/nologin -M nextcloud
+sudo useradd -u 1004 -U -s /usr/sbin/nologin -M jellyseerr
